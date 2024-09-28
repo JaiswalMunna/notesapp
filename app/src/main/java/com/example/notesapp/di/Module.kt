0c5340cf02a753450a106def2d8ccf74846a2ccd
MunnaJaiswal <codermunna007@gmail.com>
2024-09-28 22:06:48 +0545

    Working on databases

--- app/src/main/java/com/example/notesapp/di/Module.kt
+++ app/src/main/java/com/example/notesapp/di/Module.kt
@@ -1,6 +1,7 @@
 package com.example.notesapp.di
 import android.app.Application
 import androidx.room.Room
+import com.example.notesapp.data_layer.NoteDB
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -8,10 +9,11 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object Module {
-    fun provideDatabase(application :Application):NoteDatabase{
+
+    fun provideDatabase(application :Application):NoteDB{
         return Room.databaseBuilder(
             context = application,
-            NoteDatabase::class.java,
+            NoteDB::class.java,
             "note_database.sql"
         ).build()
     }
