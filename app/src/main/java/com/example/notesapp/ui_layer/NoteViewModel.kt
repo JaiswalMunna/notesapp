362909849740497ecbfebd6d0d316c6ee16698df
MunnaJaiswal <codermunna007@gmail.com>
2024-09-28 22:55:54 +0545

    event handling

--- app/src/main/java/com/example/notesapp/ui_layer/NoteViewModel.kt
+++ app/src/main/java/com/example/notesapp/ui_layer/NoteViewModel.kt
@@ -1,6 +1,58 @@
 package com.example.notesapp.ui_layer
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.notesapp.data_layer.Note
+import com.example.notesapp.data_layer.NoteDB
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class NoteViewModel:ViewModel() {
+class NoteViewModel @Inject constructor(noteDB: NoteDB) : ViewModel() {
+    val dao=noteDB.noteDao
+    private val isSortedByDataAdded= MutableStateFlow(true)
+    private var notes= isSortedByDataAdded.flatMapLatest {
+        if (it){
+            dao.getAllOrderedDateAdded()
+        }else{
+            dao.getAllOrderedTitle()
+        }
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(),
+        emptyList()
+    )
+    val _state= MutableStateFlow(NoteState())
+    val state= combine(_state,isSortedByDataAdded,notes){
+        state,isSortedByDateAdded,notes->state.copy(
+            notes = notes
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), NoteState())
+
+    fun onEvent(event:NoteEvent){
+        when(event){
+            is NoteEvent.DeleteNote->{
+                viewModelScope.launch {
+                    dao.delete(event.note)
+                }
+            }
+            is NoteEvent.SaveNote -> {
+                viewModelScope.launch {
+                    val note = Note(
+                        title = state.value.title.value,
+                        disp = state.value.disp.value,
+                        dateDated = System.currentTimeMillis()
+                    )
+                    viewModelScope.launch { dao.upsert(note) }
+                }
+            }
+            NoteEvent.SortNotes-> {
+                isSortedByDataAdded.value=!isSortedByDataAdded.value
+            }
+        }
+    }
 }
