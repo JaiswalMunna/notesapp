6c7aa97a564ca4656dad4d9b9e16e5dd41c453ff
MunnaJaiswal <codermunna007@gmail.com>
2024-09-28 21:50:55 +0545

    CRUD operation class was created

---
+++ app/src/main/java/com/example/notesapp/data_layer/Note.kt
@@ -0,0 +1,12 @@
+package com.example.notesapp.data_layer
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true) val id:Int=0,
+    var title:String,
+    var disp:String,
+    var dateDated:Long
+)
---
+++ app/src/main/java/com/example/notesapp/data_layer/NoteDB.kt
@@ -0,0 +1,5 @@
+package com.example.notesapp.data_layer
+
+import androidx.room.RoomDatabase
+
+abstract class NoteDB:RoomDatabase()
